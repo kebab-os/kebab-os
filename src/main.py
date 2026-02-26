6be7e1c734fa9bebf7633191f231ae0df71abc7a
7ames <123058@combertonvc.org>
2026-02-26 08:42:42 +0000

    Create main.py

---
+++ src/main.py
@@ -0,0 +1,12 @@
+# kebabOS v0.3.0
+# ---------------------------------
+#
+# Welcome to kebabOS! Please read the README to to make sure that
+# you have all the dependencies intalled and know how to use it.
+
+
+
+from kernel.core import boot
+
+if __name__ == "__main__":
+    boot()
