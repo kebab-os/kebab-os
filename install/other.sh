c1863534761e1e0d2ae589f83cdd3a54a794a530
7ames <123058@combertonvc.org>
2026-02-27 13:03:48 +0000

    Update other.sh

--- install/other.sh
+++ install/other.sh
@@ -1,7 +1,7 @@
 cd ~
-mkdir .kebab
+if (!(Test-Path .kebab)) { mkdir .kebab }
 cd .kebab
-curl.exe -L -O https://github.com/kebab-os/kebab-os/archive/refs/heads/main.zip
-Expand-Archive -Path kebab-os.zip -DestinationPath .
+curl.exe -L https://github.com/kebab-os/kebab-os/archive/refs/heads/main.zip -o kebab-os.zip
+Expand-Archive -Path kebab-os.zip -DestinationPath . -Force
 cd kebab-os-main
 pip install pygame requests html2image
