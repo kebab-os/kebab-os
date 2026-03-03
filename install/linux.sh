ac71f8e5ed3070e42a1350200bcae825e72a4164
7ames <123058@combertonvc.org>
2026-03-03 10:56:10 +0000

    Update linux.sh

--- install/linux.sh
+++ install/linux.sh
@@ -1,7 +1,7 @@
 cd ~
 mkdir .kebab
 cd .kebab
-git clone https://github.com/kebab-os/kebab-os.git
-cd kebab-os
+git clone https://github.com/kebab-os/kebab-gui.git
+cd kebab-gui
 sudo apt-get install xclip
 pip install pygame requests html2image
